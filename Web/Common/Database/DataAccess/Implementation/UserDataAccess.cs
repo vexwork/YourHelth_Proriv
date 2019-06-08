2eb8d483abd2024e063f987281e60b6501b556d5
Pavel <burenevpn@gmail.com>
2019-06-08 15:12:15 +0300

    Поднял пакеты до sln

--- Web/Common/Database/DataAccess/Implementation/UserDataAccess.cs
+++ Web/Common/Database/DataAccess/Implementation/UserDataAccess.cs
@@ -31,13 +31,6 @@ namespace Common.Database.DataAccess.Implementation
         void IDisposable.Dispose()
         {
             _context.Dispose();
-            return Task.FromResult(new
-                User
-                {
-                    Guid = Guid.NewGuid(),
-                    Name = "Vasya",
-                }
-            );
         }
     }
 }
