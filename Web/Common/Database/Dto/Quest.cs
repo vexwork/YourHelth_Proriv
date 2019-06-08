fbd1fa03fdef5095db4c1f7e51fc45294472728c
Pavel <burenevpn@gmail.com>
2019-06-08 21:48:30 +0300

    Поправил структуру, добавил скрипт очистки

--- Web/Common/Database/Dto/Quest.cs
+++ Web/Common/Database/Dto/Quest.cs
@@ -17,12 +17,9 @@ namespace Common.Database.Dto
         public Conquest Conquest { get; set; }
 
         [Required]
-        public DateTime BeginTime { get; set; }
-
+        public DateTime Time { get; set; }
+        
         [Required]
-        public DateTime EndTime { get; set; }
-
-        [Required]
-        public List<Prescription> Prescriptions { get; set; }
+        public Prescription Prescription { get; set; }
     }
 }
