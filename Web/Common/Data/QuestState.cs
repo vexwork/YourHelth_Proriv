5f9172e2ca4a39734bdbf9661809174a5299a8e5
Pavel <burenevpn@gmail.com>
2019-06-09 11:57:39 +0300

    Дополнил модельки конквестов

--- Web/Common/Data/QuestState.cs
+++ Web/Common/Data/QuestState.cs
@@ -1,9 +1,14 @@
-﻿namespace Common.Data
+﻿using System.Runtime.Serialization;
+
+namespace Common.Data
 {
     public enum QuestState
     {
+        [EnumMember(Value = "waiting")]
         Waiting,
+        [EnumMember(Value = "passed")]
         Passed,
+        [EnumMember(Value = "failed")]
         Failed
     }
 }
--- Web/WebApiApplication/Models/Conquests/ConquestsResponse.cs
+++ Web/WebApiApplication/Models/Conquests/ConquestsResponse.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace WebApiApplication.Models.Conquests
 {
+    [DataContract]
     public class ConquestsResponse
     {
     }
